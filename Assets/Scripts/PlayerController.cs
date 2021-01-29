4e2e9be6ef58b3a5fd4298cb31008d5b1828b12d
TheDonciuxX <bandym555@gmail.com>
2021-01-29 20:24:07 +0200

    Collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * moveSpeed * Time.deltaTime;
+        var control = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.MovePosition(transform.position * Vector2.one + control * moveSpeed * Time.deltaTime);
     }
 }
